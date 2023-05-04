6134921152a14986cf426a0142c67533d9331c11
Douglas Alves de Sousa <douglas.sousa@ccc.ufcg.edu.br>
2023-05-04 19:24:52 -0300

    adding getDifficulty function and writing function names in camel case

--- src/EasyEngine.hs
+++ src/EasyEngine.hs
@@ -1,31 +1,31 @@
-module EasyEngine (easy_engine) where 
+module EasyEngine (easyEngine) where 
 
-easy_engine :: [Int] -> [Int]
-easy_engine list = do
+easyEngine :: [Int] -> (Int,Int)
+easyEngine list = do
     
-    let idx = small_pos_heap_index list;
-    let cnt = count_positive_indexes list;
+    let idx = smallPosHeapIndex list;
+    let cnt = countPositiveIndexes list;
     let mid = list !! idx;
 
-    if cnt > 1 then [(min mid 3), idx]
-    else if mid > 3 then [1, idx]
-    else [mid, idx]
+    if cnt > 1 then ((min mid 3), idx)
+    else if mid > 3 then (1, idx)
+    else (mid, idx)
 
-count_positive_indexes :: [Int] -> Int
-count_positive_indexes [] = 0
-count_positive_indexes (x:xs) = do
+countPositiveIndexes :: [Int] -> Int
+countPositiveIndexes [] = 0
+countPositiveIndexes (x:xs) = do
     if x > 0 then
-        1 + count_positive_indexes xs
+        1 + countPositiveIndexes xs
     else
-        count_positive_indexes xs
+        countPositiveIndexes xs
 
-small_pos_value :: [Int] -> Int
-small_pos_value = minimum . filter (>0)
+smallPosValue :: [Int] -> Int
+smallPosValue = minimum . filter (>0)
 
-small_pos_index :: Int -> [Int] -> Int -> Int
-small_pos_index small (x:xs) i = do
+smallPosIndex :: Int -> [Int] -> Int -> Int
+smallPosIndex small (x:xs) i = do
     if x == small then i
-    else small_pos_index small xs (i+1)
+    else smallPosIndex small xs (i+1)
 
-small_pos_heap_index :: [Int] -> Int
-small_pos_heap_index list = small_pos_index (small_pos_value list) list 0
+smallPosHeapIndex :: [Int] -> Int
+smallPosHeapIndex list = smallPosIndex (smallPosValue list) list 0
--- src/HardEngine.hs
+++ src/HardEngine.hs
@@ -1,23 +1,23 @@
-module HardEngine (hard_engine) where 
+module HardEngine (hardEngine) where 
 import Data.Bits
 
-hard_engine :: [Int] -> [Int]
-hard_engine list = do
-    let xor4 = get_xor_mod4 list;
-    if xor4 == 0 then make_random_move list 0
-    else make_best_move xor4 list 0
+hardEngine :: [Int] -> (Int,Int)
+hardEngine list = do
+    let xor4 = getXorMod4 list;
+    if xor4 == 0 then makeRandomMove list 0
+    else makeBestMove xor4 list 0
 
-get_xor_mod4 :: [Int] -> Int
-get_xor_mod4 [] = 0
-get_xor_mod4 (x:xs) = xor (rem x 4) (get_xor_mod4 xs)
+getXorMod4 :: [Int] -> Int
+getXorMod4 [] = 0
+getXorMod4 (x:xs) = xor (rem x 4) (getXorMod4 xs)
 
-make_random_move :: [Int] -> Int -> [Int]
-make_random_move (0:tail) i = make_random_move tail (i+1)
-make_random_move (x:tail) i = [(min x 3), i]
+makeRandomMove :: [Int] -> Int -> (Int,Int)
+makeRandomMove (0:tail) i = makeRandomMove tail (i+1)
+makeRandomMove (x:tail) i = ((min x 3), i)
 
-make_best_move :: Int -> [Int] -> Int -> [Int]
-make_best_move xOr (x:xs) i =
-    if x-1 >= 0 && xor (xor xOr x) (x-1) == 0 then [1, i]
-    else if x-2 >= 0 && xor (xor xOr x) (x-2) == 0 then [2, i]
-    else if x-3 >= 0 && xor (xor xOr x) (x-3) == 0 then [3, i]
-    else make_best_move xOr xs (i+1)
+makeBestMove :: Int -> [Int] -> Int -> (Int,Int)
+makeBestMove xOr (x:xs) i =
+    if x-1 >= 0 && xor (xor xOr x) (x-1) == 0 then (1, i)
+    else if x-2 >= 0 && xor (xor xOr x) (x-2) == 0 then (2, i)
+    else if x-3 >= 0 && xor (xor xOr x) (x-3) == 0 then (3, i)
+    else makeBestMove xOr xs (i+1)
