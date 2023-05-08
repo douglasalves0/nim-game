7a4d2c62553d8a1f422105cc9ab1cd72da28645c
MayaBrito <mayara.pinheiro@ccc.ufcg.edu.br>
2023-05-08 13:37:09 -0300

    Removing two functions that are not used anymore

--- src/Validate/ValidateMenuInput.hs
+++ src/Validate/ValidateMenuInput.hs
@@ -1,20 +1,13 @@
-module Validate.ValidateMenuInput (validateMenuInput, validateInput,validateRange) where
+module Validate.ValidateMenuInput where
 
 import Data.Char (isDigit)
 
-validateMenuInput :: String -> Bool
-validateMenuInput option
-    | not (all isDigit option) = False
-    | (read option :: Int) < 1 || (read option :: Int) > 4 = False
-    | otherwise = True
-
-validateInput :: String -> Bool
-validateInput option
-    | not (all isDigit option) = False
-    | (read option :: Int) < 1 || (read option :: Int) > 2 = False
-    | otherwise = True
-
-validateRange ::Int -> String -> Bool
+validateRange :: Int -> String -> Bool
 validateRange size option
-    | not (all isDigit option) = False
-    | otherwise = (read option :: Int) `elem` [1..size]
+  | not (all isDigit option) = False
+  | otherwise = (read option :: Int) `elem` [1 .. size]
+
+validateName :: String -> Bool
+validateName name
+  | name == "" = False
+  | otherwise = True
