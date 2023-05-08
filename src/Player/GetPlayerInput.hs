2b1f07599fd6836580fccd41052831b27fe83572
MayaBrito <mayara.pinheiro@ccc.ufcg.edu.br>
2023-05-08 13:35:24 -0300

    Refactoring GetPlayerInput and StartGame so they use StringsAndCommand

--- src/Player/GetPlayerInput.hs
+++ src/Player/GetPlayerInput.hs
@@ -1,17 +1,24 @@
 module Player.GetPlayerInput where
-import Validate.ValidatePlayerInput
+
 import GeniusDraw.DrawingFunctions (drawGameLoop)
+import Interface.StringsAndCommand (clearT, takeCoins, takeStack)
+import Validate.ValidatePlayerInput
 
 getPlayerInput :: [Int] -> String -> IO (Int, Int)
 getPlayerInput stack frase1 = do
-    drawGameLoop stack frase1
-    x <- getLine
+  clearT
+  drawGameLoop stack frase1
+  x <- getLine
 
-    drawGameLoop stack "Digite a pilha de que voce quer tirar as moedas:"
-    playerStackMove <- getLine
+  clearT
+  drawGameLoop stack takeStack
+  playerStackMove <- getLine
 
-    drawGameLoop stack "Digite a quantidade de moedas que voce quer retirar:"
-    playerCoinMove <- getLine
+  clearT
+  drawGameLoop stack takeCoins
+  playerCoinMove <- getLine
 
-    let validation = validatePlayerInput playerCoinMove playerStackMove stack
-    if validation == "" then return (read playerCoinMove, read playerStackMove - 1) else getPlayerInput stack validation
+  let validation = validatePlayerInput playerCoinMove playerStackMove stack
+  if validation == ""
+    then return (read playerCoinMove, read playerStackMove - 1)
+    else getPlayerInput stack validation
