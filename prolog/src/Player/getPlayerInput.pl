04d18ac5d43a982bb658af4c3127fbe66bcaac42
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-06-07 16:53:15 -0300

    refactor: juntando os get dos inputs em uma função só

--- prolog/src/Player/getPlayerInput.pl
+++ prolog/src/Player/getPlayerInput.pl
@@ -1,35 +1,38 @@
-validatePlayerInputCoins(PlayerInputCoins):-
-    (is_of_type(text, PlayerInputCoins) ; is_of_type(char, PlayerInputCoins) ; PlayerInputCoins < 1 ; PlayerInputCoins > 3) ->
-    (
-        write("Digite um numero inteiro entre 1 e 3\n"),
-        getPlayerInputCoins()
-    ) ;
-    write(PlayerInputCoins).
-
-validatePlayerInputStacks(PlayerInputStacks):- 
-    (is_of_type(negative_integer, PlayerInputStacks) ; is_of_type(char, PlayerInputStacks) ; is_of_type(text, PlayerInputStacks)) ->
-    (
-        write("Digite um numero inteiro maior que 0\n"),
-        getPlayerInputStacks()
-    ) ; 
-    write(PlayerInputStacks).
-
-getPlayerInputCoins():-
-    write("Quantas moedas voce quer retirar?\n"),
+getPlayerInputCoins(InputCoins):-
+    write("Quantas moedas voce quer retirar?\n"),    
     read(PlayerInputCoins),
+    (
+        (is_of_type(text, PlayerInputCoins) ; is_of_type(char, PlayerInputCoins) ; PlayerInputCoins < 1 ; PlayerInputCoins > 3) ->
+            (
+                write("Digite um numero inteiro entre 1 e 3\n"),
+                getPlayerInputCoins(InputCoins)
+            )
+        ;
+        InputCoins is PlayerInputCoins
+    ).
+
+getPlayerInputStacks(InputStack):- 
+    write("De qual pilha voce quer retirar as moedas?\n"), 
+    read(PlayerInputStacks),
+    (
+        (is_of_type(negative_integer, PlayerInputStacks) ; is_of_type(char, PlayerInputStacks) ; is_of_type(text, PlayerInputStacks) ; PlayerInputStacks = 0) ->
+            (
+                write("Digite um numero inteiro maior que 0\n"),
+                getPlayerInputStacks(InputStack)
+            )
+        ; 
+        InputStack is PlayerInputStacks    
+    ).
+
+deleteFromStack(InputCoins, InputStack):-
+    write(InputCoins),
     nl,
-    validatePlayerInputCoins(PlayerInputCoins),
-    
-    getPlayerInputStacks(),
+    write(InputStack),
+    nl,
     halt.
 
-getPlayerInputStacks():-
-    write("De qual pilha voce quer retirar as moedas?\n"),
-    read(PlayerInputStacks),
-    nl,
-    validatePlayerInputStacks(PlayerInputStacks),
-  
-    halt.% Aqui chama a funcao de deletar da pilha
+getPlayerInput():-
+    getPlayerInputCoins(InputCoins),
+    getPlayerInputStacks(InputStack),
 
-
-
+    deleteFromStack(InputCoins, InputStack).
