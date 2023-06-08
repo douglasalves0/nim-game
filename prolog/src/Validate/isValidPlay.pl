9bd16ac97ea225729575f911caa0254c0fdb8154
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-06-08 17:03:18 -0300

    refactor: alterando logica de validacao de inputs

--- prolog/src/Validate/isValidPlay.pl
+++ prolog/src/Validate/isValidPlay.pl
@@ -1,6 +1,44 @@
-isValidPlay(CoinsQuantity, IndexStack, Stack):-
-  length(Stack, Size),
+:- module(validation, [isValidPlay/3]).
+
+isValidPlay(CoinsQuantity, IndexStack, StackList):-
+  (                                                                                                                              
+    (is_of_type(text, PlayerInputCoins) ; is_of_type(char, PlayerInputCoins) ; PlayerInputCoins < 1 ; PlayerInputCoins > 3) ->   
+      (                                                                                                                        ) 
+        write("Digite um numero inteiro entre 1 e 3\n"),                                                                         
+        getPlayerInput(StackList)                                                                                                
+      )                                                                                                                          
+    ;                                                                                                                            
+    true                                                                                                                         
+  ),
 
   (
-    (IndexStack > Size) -> write("Digite uma pilha valida")
-  ) 
+    (is_of_type(negative_integer, PlayerInputStacks) ; is_of_type(char, PlayerInputStacks) ; is_of_type(text, PlayerInputStacks) ; PlayerInputStacks = 0) -> 
+      (                                                                                                                                                    )
+        write("Digite um numero inteiro maior que 0\n"),                                                                                                 
+        getPlayerInputStacks(InputStack)                                                                                                                 
+      )                                                                                                                                                    
+    ;
+    true
+  ),
+
+  length(StackList, Size),
+  ( % Verificacao se o index da lista de pilhas eh valido
+    (IndexStack > Size) -> 
+      (
+        write("Digite uma pilha valida para retirar as moedas"),
+        getPlayerInput(StackList)
+      )
+    ;
+    true
+  ),
+
+  nth0(IndexStack - 1, StackList, Stack),
+  ( % Verificacao se a quantidade de moedas do player eh maior que a pilha desejada
+    (CoinsQuantity > Stack) ->
+      (
+        write("A quantidade de moedas que voce quer retirar eh maior do que a pilha"),
+        getPlayerInput(StackList)
+      )
+    ;
+    true
+  ).
