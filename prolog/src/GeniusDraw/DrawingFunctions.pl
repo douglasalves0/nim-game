ed7b91dd5a3e9d15beacb0a96e8971b42a686470
guilherme Silva Toledo <guilherme.toledo+github@ccc.ufcg.edu.br>
2023-06-09 00:58:36 -0300

    base desenho genio

---
+++ prolog/src/GeniusDraw/DrawingFunctions.pl
@@ -0,0 +1,154 @@
+getGennie(1,'\\|  /}      ').
+getGennie(2,'./`|\'\\     ').
+getGennie(3,'|_~?-_`\\   ').
+getGennie(4,'-^O `>/   ').
+getGennie(5,'~~~ /_    ').
+getGennie(6,'- )\'/    \'- ').
+getGennie(7,',-\' _ _    /  \\').
+getGennie(8,'>-\'\'\'  ~`\\_/   |').
+getGennie(9,'( _ .\'  `-\'  -\'/').
+getGennie(10,'@                  \`\'\'  \`-_   _// ').
+getGennie(11,'( )                   |     \'\'\'_/  ').
+getGennie(12,'/===\\                   \\      /    ').
+getGennie(13,',-`-._.-\'\'\'\'\' \'-_..---.           \\     |    ').
+getGennie(14,'((\\\'\',\'\\ \'-._____.--\' .---\\..^,     .,)    /    ').
+getGennie(15,'\\\'-*^\\::          /     \'{  3   {\'.:   /     ').
+getGennie(16,'\\    _    /          \'~.\'\'y`,\'\\~\'      ').
+getGennie(17,'\'\'\\- -/\'\'              \'-.{ .3\'       ').
+getGennie(18,'_,-\' `-_                   \'          ').
+getGennie(19,'`-------\'                             ').
+
+% desenhar o genio
+makeSpace(0):- write("").
+makeSpace(N):-
+    N2 is N - 1,
+    write(" "),
+    makeSpace(N2).
+
+fillGenie(_,0):- write("").
+
+fillGenie(N,GenieSize):-
+    NextGenie is GenieSize - 1,
+    fillGenie(N,NextGenie),
+    getGennie(GenieSize,Genie),
+    atom_chars(Genie,GenieList),
+    length(GenieList,Length),
+    makeSpace(N-Length),write(Genie),nl.
+
+%desenhar o genio com suas pilhas
+drawStack() :-
+    makeSpace(2),
+    write("[--]").
+
+fillStackGenie([_],N):-
+    drawStack(),
+    getGennie(19,Genie),
+    atom_chars(Genie,GenieList),
+    length(GenieList,Length),
+    Nspace is N - Length - 6 -1,
+    makeSpace(Nspace),write(Genie),nl.
+
+fillStackGenie([_|T],N):-
+    drawStack(),
+    NextN is N - 6,
+    fillStackGenie(T,NextN).
+
+drawCoins(X,N) :-
+    makeSpace(4),
+    (X >= 19-N -> write("$$") ; write("  ")).
+
+fillCoinsGenie(L,N,19):-
+    makeSpace(1),
+    fillStackGenie(L,N).
+
+fillCoinsGenie([H],N,GenieSize):-
+    drawCoins(H,GenieSize),
+    getGennie(GenieSize,Genie),
+    atom_chars(Genie,GenieList),
+    length(GenieList,Length),
+    Nspace is N - Length - 6,
+    makeSpace(Nspace),write(Genie),nl.
+
+fillCoinsGenie([H|T],N,GenieSize):-
+    drawCoins(H,GenieSize),
+    NextN is N - 6,
+    fillCoinsGenie(T,NextN,GenieSize).
+
+coinDrawLoop(_,_,0):- write(""),!.
+
+coinDrawLoop(L,N,GenieSize):-
+    NextGenie is GenieSize - 1,
+    coinDrawLoop(L,N,NextGenie),
+    fillCoinsGenie(L,N,GenieSize).
+    
+% desenhar a fala do genio
+topSpeechBubble(0):- write("\\"),nl.
+
+topSpeechBubble(N) :-
+    N2 is N - 1,
+    write("-"),
+    topSpeechBubble(N2).
+
+startTopSpeechBubble(N):- 
+    write("/"),
+    topSpeechBubble(N).
+
+bottomSpeechBubble(0):- write('_ \\'),nl,!.
+
+bottomSpeechBubble(N) :-
+    N2 is N - 1,
+    write("-"),
+    bottomSpeechBubble(N2).
+
+startLine(Size,[]) :-
+    write('|'),
+    bottomSpeechBubble(Size).
+
+startLine(Size,LText) :-
+    write('|'),
+    writeLine(Size,Size,LText),
+    write('|'),
+    bottomSpeechBubble(Size-1).
+
+writeLine(_,Size,[]) :-
+    makeSpace(Size),write('|'),!.
+
+writeLine(_,Size,[X]) :-
+    atom_chars(X,WList),
+    length(WList,Length),
+    Nsize is Size - Length,
+    write(X),
+    makeSpace(Nsize),write('|'),nl,!.
+
+writeLine(Max,Size,[X|T]) :-
+    atom_chars(X,WList),
+    length(WList,Length),
+    Nsize is Size - Length -1,
+    ( Nsize >= 0  ->
+        (write(X),write(" "), writeLine(Max, Nsize,T)) ;
+        makeSpace(Size),write('|'),nl,write('|'),writeLine(Max,Max,[X|T])
+        ).
+
+% funcoes para desenhar o genio
+drawStartGenie(Text) :- 
+    SpeechBubbleSize = 96,
+    GenieSize = 19,
+    LineWidth = 110,
+    split_string(Text, ' ', '', LText),
+    startTopSpeechBubble(SpeechBubbleSize),
+    startLine(SpeechBubbleSize,LText),
+    fillGenie(LineWidth,GenieSize).
+
+% drawMenuGenie()
+
+% drawHintGenie()
+
+drawGameLoop(Stack,Text) :-
+    SpeechBubbleSize = 96,
+    GenieSize = 19,
+    LineWidth = 110,
+    split_string(Text, ' ', '', LText),
+    startTopSpeechBubble(SpeechBubbleSize),
+    startLine(SpeechBubbleSize,LText),
+    coinDrawLoop(Stack,LineWidth,GenieSize).
+
