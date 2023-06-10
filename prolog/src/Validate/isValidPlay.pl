6a9ecdd4e177e14695b3cb3356a70e2d6cd9e7e1
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-06-10 18:42:53 -0300

    refactor: refatorando validacao

--- prolog/src/Validate/isValidPlay.pl
+++ prolog/src/Validate/isValidPlay.pl
@@ -1,7 +1,13 @@
-isValidPlay(CoinsQuantity, IndexStack, StackList):-
-  number(IndexStack),
-  number(CoinsQuantity),
-  CoinsQuantity < 4,
-  CoinsQuantity > 0,
+:- include('../Interface/StringsAndCommands').
+
+isValidPlay(CoinsQuantity, IndexStack, StackList, Msg):-
+  number(IndexStack) -> Msg is presentsValidEntryNumber,
+  number(CoinsQuantity) Msg is presentsValidEntryPositive,
+  CoinsQuantity > 3 -> Msg is presentsValidEntryCoins,
+  CoinsQuantity < 1 -> Msg is presentsValidEntryCoins,
+
   nth0(IndexStack, StackList, V),
-  V >= CoinsQuantity.
+  V <= CoinsQuantity -> Msg is presentsValidEntryStackCoins,
+
+  nonvar(Msg) -> Msg is "" ; true.
+  
