82fe427a1e5d8a52defd4814ca41595aec98c3f6
Maya Brito <mayara.pinheiro@ccc.ufcg.edu.br>
2023-06-11 12:36:44 -0300

    Atualizando o mainMenu.pl

--- prolog/src/Interface/mainMenu.pl
+++ prolog/src/Interface/mainMenu.pl
@@ -3,26 +3,26 @@
 :- include('../Interface/StringsAndCommand.pl').
 :- include('../Validate/isValidMenu.pl').
 
-initGame(OpeningSentence, Size) :-
+initGame(OpeningSentence) :-
   clearT,
   initMenuTitle(Title),
   initMenuOptions(Options),
   drawMenuGenie(OpeningSentence, Title, Options),
-  read_line_to_string(user_input, OptionString),
-  number_string(Option, OptionString),
-  (validateRange(Size, Option) ->
+  read(Option),
+  (
+    validateRange(2, Option) ->
     (Option =:= 1 ->
-      presentsMainMenu(MainMenuMessage),
-      mainMenu(MainMenuMessage)
+      (presentsMainMenu(MainMenuMessage),
+       mainMenu(MainMenuMessage))
     ;
       Option =:= 2 ->
-      clearT,
-      presentsInitialWithdrawal(WithdrawalMessage),
-      drawStartGenie(WithdrawalMessage)
+      ( clearT,
+        presentsInitialWithdrawal(WithdrawalMessage),
+        drawStartGenie(WithdrawalMessage))
     )
   ;
     presentsValidEntry(ValidEntryMessage),
-    initGame(ValidEntryMessage, Size)
+    initGame(ValidEntryMessage)
   ).
 
 mainMenu(OpeningSentence) :-
@@ -30,57 +30,67 @@ mainMenu(OpeningSentence) :-
   mainMenuTitle(Title),
   mainMenuOptions(Options),
   drawMenuGenie(OpeningSentence, Title, Options),
-  read_line_to_string(user_input, Option),
+  read(Option),
   (validateRange(4, Option) ->
-    atom_number(Option, OptionInt),
-    getMenu(OptionInt)
+    getMenu(Option, OpeningSentence)
   ;
-    presentsValidEntry(ValidEntryMessage),
-    mainMenu(ValidEntryMessage)
+    (call(presentsValidEntry, ValidEntryMessage),
+    mainMenu(ValidEntryMessage))
   ).
 
-getMenu(Option) :-
-  Option = 1,
+getMenu(1, OpeningSentence) :-
   clearT,
-  drawMenuGenie(PresentsOpponents, OpponentsMenuTitle, OpponentsMenuOptions),
-  read_line_to_string(user_input, Option1),
+  call(PresentsOpponents, PresentsOp),
+  call(OpponentsMenuTitle, OpponentsMT),
+  call(OpponentsMenuOptions, OpponentsMO),
+  drawMenuGenie(PresentsOp, OpponentsMT, OpponentsMO),
+  read(Option1),
   (validateRange(2, Option1) ->
-    (OptionInt is integer(Option1),
-     (OptionInt =:= 1 ->
-       getNamePlayer(PresentsNamePlayer1, NamePlayer1),
-       getNamePlayer(PresentsNamePlayer2, NamePlayer2),
+     (Option1 =:= 1 ->
+       call(presentsNamePlayer1, Player1),
+       call(presentsNamePlayer2, Player2),
+       getNamePlayer(Player1, NamePlayer1),
+       getNamePlayer(Player2, NamePlayer2),
        createStack(4, Stack),
        gameLoop(Stack, 0, NamePlayer1, NamePlayer2)
      ;
-       createDifficulty(PresentsChallenges)
-     )
+       call(PresentsChallenges, challenges),
+       createDifficulty(challenges)
     ),
-    getMenu(1)
+    call(PresentsMainMenu, main),
+    mainMenu(main)
   ;
-    getMenu(1)
+    getMenu(1, OpeningSentence)
   ).
-getMenu(Option) :-
-  Option = 2,
+
+getMenu(2, OpeningSentence) :-
   clearT,
-  drawHintGenie(PresentsChallengesTypes, ChallengeTypes),
-  read_line_to_string(user_input, _),
-  mainMenu(PresentsMainChallenge).
-getMenu(Option) :-
-  Option = 3,
+  call(PresentsChallengesTypes, PresentsCT),
+  call(ChallengeTypes, ChallengeT),
+  drawHintGenie(PresentsCT, ChallengeT),
+  read(Option),
+  call(PresentsChallenges, challenges),
+  mainMenu(challenges).
+
+getMenu(3, OpeningSentence) :-
   clearT,
-  drawHintGenie(PresentsChallengesPerforms, PerformsChallenge),
-  read_line_to_string(user_input, _),
+  call(PresentsChallengesPerforms, PresentsCP),
+  call(PerformsChallenge, PerformsC),
+  drawHintGenie(PresentsChallengesPerforms, PerformsC),
+  read(Option),
   mainMenu(PresentsMainPerforms).
-getMenu(Option) :-
-  Option = 4,
+
+getMenu(4, OpeningSentence) :-
   clearT,
-  drawStartGenie(PresentsWithdrawal).
+  call(PresentsWithdrawal, PresentsW),
+  drawStartGenie(PresentsW).
 
 getNamePlayer(Phrase, Name) :-
   clearT,
   drawStartGenie(Phrase),
-  read_line_to_string(user_input, Name),
-  (validateName(Name) -> true ; getNamePlayer(PresentsNameError, Name)).
+  read(user_input, Name),
+  call(PresentsNameError, NameError),
+  (validateName(Name) -> true ; getNamePlayer(NameError, Name)).
 
 createDifficulty(Phrase) :-
   clearT,
@@ -90,26 +100,26 @@ createDifficulty(Phrase) :-
   read_line_to_string(user_input, Option2),
   (validateRange(3, Option2) ->
     atom_number(Option2, Difficulty),
-    getNamePlayer(PresentsNamePlayer, Name),
+    call(PresentsNamePlayer, PhraseNamePlayer),
+    getNamePlayer(PhraseNamePlayer, Name),
     createStack(Difficulty, Stack),
     gameLoop(Stack, Difficulty, Name, "")
   ;
-    createDifficulty(PresentsValidEntry)
+    call(PresentsValidEntry, PhraseValidEntry),
+    createDifficulty(PhraseValidEntry)
   ).
 
-createStack(Difficulty, Stack) :-
-  Difficulty =:= 1,
+createStack(1, Stack) :-
   getStacks(10, 3, Stack).
-createStack(Difficulty, Stack) :-
-  Difficulty =:= 2,
-  getStacks(15, 4, Stack).
-createStack(Difficulty, Stack) :-
-  Difficulty =:= 3,
-  getStacks(20, 5, Stack).
-createStack(Difficulty, Stack) :-
-  Difficulty =:= 4,
-  getStacks(15, 5, Stack).
 
+createStack(2, Stack) :-
+  getStacks(15, 4, Stack).
+
+createStack(3, Stack) :-
+  getStacks(20, 5, Stack).
+
+createStack(4, Stack) :-
+  getStacks(15, 5, Stack).
 
 getStacks(Coins, 1, [Coins]) :- !.
 
